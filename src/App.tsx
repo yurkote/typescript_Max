dbea61cb20d63bd54d644e85e7f3235e4e4a89a6
yurkote <yurokte@gmail.com>
2026-02-16 20:32:03 +0100

    Generic props func component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <Todos
+          items={[
+            { id: "1", text: "Learn React" },
+            { id: "2", text: "Learn TypeScript" },
+          ]}
+        />
+      </div>
     </>
   );
 }
