729f596e51d7f5d90c7b052b7672e082827ee5b4
yurkote <yurokte@gmail.com>
2026-02-17 10:31:10 +0100

    todo item with ts

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,13 +1,14 @@
 import type Todo from "./models/todo";
+import TodoItem from "./TodoItem";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
-}
+};
 
 export default Todos;
